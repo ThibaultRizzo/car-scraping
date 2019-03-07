4fb3ed9834a76118d6dae5328972f6ba0a59d8c2
RIZZO Thibault <rizzo@RIZZOs-MacBook-Air.local>
2019-03-07 15:19:38 +0100

    Delete comments

--- team-pack-dev/settings.py
+++ team-pack-dev/settings.py
@@ -106,8 +106,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 SITE_ID = 1
 
